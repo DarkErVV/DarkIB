704e5e5579b217a1c80d8921c3c1117bee669b64
DarkEr <darkeragent@yandex.ru>
2017-05-15 11:40:49 +0300

    new file

--- darkib/app/__init__.py
+++ darkib/app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask.ext.sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
@@ -7,6 +7,7 @@ app = Flask(__name__)
 app.config.from_object('config')
 #app.config.from_object('database_conf')
 
+#Database initialisation
 db = SQLAchemy(app)
 
 from app import views, models
