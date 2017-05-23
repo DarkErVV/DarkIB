b250fa2b21e4d8e5530195c3b891f10da352d733
DarkEr <darkeragent@yandex.ru>
2017-05-23 13:55:54 +0300

    Edit Templates.

--- darkib/app/views.py
+++ darkib/app/views.py
@@ -6,6 +6,10 @@ from app import app
 def index():
     return  render_template("index.html")
 
+@app.route('/new')
+def new_img():
+    return  render_template("new_img.html")
+
 @app.route('/about')
 def about():
     return render_template('about.html')
